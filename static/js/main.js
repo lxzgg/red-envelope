fa883513b8b428a4a32e86456f5b31172620d9c3
lxzgg <471227142@qq.com>
2018-10-11 20:21:44 +0800

    commit

--- static/js/main.js
+++ static/js/main.js
@@ -15,7 +15,10 @@ function ajax(options) {
 
   xhr.open(method, url, true)
 
-  if (data && !isFormData) xhr.setRequestHeader('Content-Type', 'application/json')
+  if (data && !isFormData) {
+    data = JSON.stringify(data)
+    xhr.setRequestHeader('Content-Type', 'application/json')
+  }
 
   for (let i in header) xhr.setRequestHeader(i, header[i])
 
@@ -33,44 +36,90 @@ function ajax(options) {
     if (complete) complete.call(this, result)
   }
 
-  if (!isFormData) data = JSON.stringify(data)
-
   xhr.send(data)
+}
+
+ajax.get = function (url, fn) {
+  this({
+    url,
+    success: function (res) {
+      fn.call(this, res)
+    },
+    fail: function (res) {
+      fn.call(this, null, res)
+    },
+  })
+}
+
+ajax.post = function (url, data, fn) {
+  this({
+    url,
+    data,
+    method: 'POST',
+    success: function (res) {
+      fn.call(this, res)
+    },
+    fail: function (res) {
+      fn.call(this, null, res)
+    },
+  })
+}
+
+// 提示
+function prompt(options) {
+  const message = options.message
+  const cancel = options.cancel
+  const confirm_click = options.success
+  const cancel_click = options.fail
+
+  const dialog = `
+<section id="dialog">
+  <div class="mask"></div>
+  <div class="dialog">
+    <div class="dialog-content">
+      ${message}
+    </div>
+    <div class="dialog-btn">
+      <a href="#" class="btn btn-default" id="cancel_confirm">${cancel ? '取消' : ''}</a>
+      <a href="#" class="btn btn-primary" id="dialog_confirm">确定</a>
+    </div>
+  </div>
+</section>`
+
+  const dialog_box = document.createElement('div')
+  dialog_box.innerHTML = dialog
+
+  const _confirm = dialog_box.querySelector('#dialog_confirm')
+  const _cancel = dialog_box.querySelector('#cancel_confirm')
+
+  _cancel.addEventListener('click', cancel_click)
+  _cancel.onclick = function () {
+    dialog_box.remove()
+    _cancel.removeEventListener('click', cancel_click)
+  }
+
+  _confirm.addEventListener('click', confirm_click)
+  _confirm.onclick = function () {
+    dialog_box.remove()
+    _confirm.removeEventListener('click', confirm_click)
+  }
+
+  document.body.appendChild(dialog_box)
+}
+
+window.onload = function () {
 
 }
 
-const formData = new FormData()
+// 提现
+function withdraw() {
 
-formData.append('a', '1')
+  prompt({
+    message: '系统不支持1元以下提现',
+    cancel: true,
+    success: function () {
+      console.log(2)
+    },
+  })
 
-ajax({
-  url: 'http://127.0.0.1:3000',
-  data: formData,
-  method: 'POST',
-  success: function (res) {
-    console.log(res)
-  },
-})
-
-//
-// ajax.post('http://127.0.0.1:3000', {a: ['1', '2']}, function (res) {
-//   console.log(res)
-// })
-//
-// ajax.get('https://cnodejs.org/api/v1/topics', function (res) {
-//   console.log(res)
-// })
-
-// window.onload = function () {
-//
-// }
-//
-// // 隐藏对话框
-// function showDialog() {
-//   document.getElementById('dialog').style.display = 'block'
-// }
-//
-// // 隐藏对话框
-// function hideDialog() {
-//   document.getElementById('dialog').style.display = 'none'
-// }
+}
